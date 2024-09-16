d7f32f79b18fadaccf50ca9fa49c8467bae14172
Connor Boyle <connor.bo@gmail.com>
2024-09-16 11:58:57 -0700

    Fix formatting

--- endpoint.py
+++ endpoint.py
@@ -2,19 +2,18 @@ import enum
 import json
 from http import HTTPStatus
 from pathlib import Path
-from typing import Any, List, Dict
+from typing import Any, Dict, List
 
 import numpy as np
 import torch.cuda
 import yaml
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
 from PIL import Image
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from pydantic import BaseModel, Field
 from ray import serve
 from ray.serve import Application
-
 from sentence_transformers import SentenceTransformer
-from transformers import pipeline, AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer, pipeline
 
 app = FastAPI()
 
@@ -91,38 +90,42 @@ class MiniCPMDeployment:
         self.task = task
         self.trust_remote_code = trust_remote_code
 
-        model = AutoModel.from_pretrained(model_path, trust_remote_code=trust_remote_code)
+        model = AutoModel.from_pretrained(
+            model_path, trust_remote_code=trust_remote_code
+        )
         model = model.to(device="cuda" if torch.cuda.is_available() else "cpu")
         self.model = model
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_path, trust_remote_code=True
+        )
 
     @app.post("/image_infer")
     async def image_infer(
         self,
-        messages: str = Form(...),  # Accept the list of dictionaries as JSON string in form data
-        image_file: UploadFile = File(...)
+        messages: str = Form(
+            ...
+        ),  # Accept the list of dictionaries as JSON string in form data
+        image_file: UploadFile = File(...),
     ):
         # Deserialize the JSON string into a list of dictionaries
         try:
             messages_list = json.loads(messages)  # Convert the JSON string to a list
         except json.JSONDecodeError:
-            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Invalid JSON format for messages")
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail="Invalid JSON format for messages",
+            )
 
         print(f"{messages_list=}")
         print(f"{image_file=!r}")
         img = Image.open(image_file.file).convert("RGB")
 
-        return self.model.chat(
-            image=img,
-            msgs=messages_list,
-            tokenizer=self.tokenizer
-        )
+        return self.model.chat(image=img, msgs=messages_list, tokenizer=self.tokenizer)
 
     @app.get("/model_config")
     def model_config(self):
         return numpy_to_std(self.model.config.__dict__)
-
 
     @app.get("/model_device")
     def model_device(self) -> str:
@@ -154,11 +157,22 @@ class SentenceTransformersModelDeployment:
 
 def deployment(args) -> Application:
     config = yaml.safe_load(open(Path(args["model_path"]) / "config.json", "r"))
-    if args["library"] == "transformers" and args["task"] == "visual-question-answering" and "auto_map" in config and config["auto_map"]["AutoConfig"].endswith("MiniCPMVConfig"):
-        return MiniCPMDeployment.bind(args["model_path"], args["task"], args["trust_remote_code"])
+    if (
+        args["library"] == "transformers"
+        and args["task"] == "visual-question-answering"
+        and "auto_map" in config
+        and config["auto_map"]["AutoConfig"].endswith("MiniCPMVConfig")
+    ):
+        return MiniCPMDeployment.bind(  # type: ignore[attr-defined]
+            args["model_path"], args["task"], args["trust_remote_code"]
+        )
     elif args["library"] == "transformers":
-        return TransformersModelDeployment.bind(args["model_path"], args["task"], args["trust_remote_code"])
+        return TransformersModelDeployment.bind(  # type: ignore[attr-defined]
+            args["model_path"], args["task"], args["trust_remote_code"]
+        )
     elif args["library"] == "sentence-transformers":
-        return SentenceTransformersModelDeployment.bind(args["model_path"], args["trust_remote_code"])
+        return SentenceTransformersModelDeployment.bind(  # type: ignore[attr-defined]
+            args["model_path"], args["trust_remote_code"]
+        )
     else:
         raise ValueError(f"Library '{args['library']}' not supported")

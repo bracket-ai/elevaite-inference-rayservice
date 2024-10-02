7093c37c7228a8a9ced6463c4f28e4479b7327cc
Korin Thompson <42357484+korinayo@users.noreply.github.com>
2024-10-02 08:20:55 -0700

    sentence transformers: eval, no grad, torch dtype

--- sentence_transformers_model.py
+++ sentence_transformers_model.py
@@ -4,7 +4,7 @@ from ray import serve
 from ray.serve import Application
 from sentence_transformers import SentenceTransformer
 
-from utils import InferenceRequest, numpy_to_std
+from utils import InferenceRequest, numpy_to_std, dtype_mapping
 
 web_app = FastAPI()
 
@@ -12,14 +12,23 @@ web_app = FastAPI()
 @serve.deployment
 @serve.ingress(web_app)
 class SentenceTransformersModelDeployment:
-    def __init__(self, model_path: str, trust_remote_code: bool):
+    def __init__(self, model_path: str, trust_remote_code: bool, torch_dtype: str = None):
         self.model_path = model_path
         self.trust_remote_code = trust_remote_code
-        self.model = SentenceTransformer(
-            self.model_path,
-            trust_remote_code=self.trust_remote_code,
-            device="cuda" if torch.cuda.is_available() else "cpu",
-        )
+        self.torch_dtype = torch_dtype
+
+        model_args = {
+            "model_name_or_path": self.model_path,
+            "trust_remote_code": self.trust_remote_code,
+            "device": "cuda" if torch.cuda.is_available() else "cpu",
+        }
+
+        if self.torch_dtype:
+            model_kwargs = {"torch_dtype": dtype_mapping.get(self.torch_dtype.lower(), None)}
+            model_args['model_kwargs'] = model_kwargs
+
+        self.model = SentenceTransformer(**model_args)
+        self.model = self.model.eval()
 
     @web_app.get("/model_device")
     def model_device(self) -> str:
@@ -29,8 +38,8 @@ class SentenceTransformersModelDeployment:
     def infer(self, inference_request: InferenceRequest) -> dict:
         args = inference_request.args
         kwargs = inference_request.kwargs
-        return {"result": numpy_to_std(self.model.encode(*args, **kwargs))}
-
+        with torch.no_grad():
+            return {"result": numpy_to_std(self.model.encode(*args, **kwargs))}
 
 def app_builder(args: dict) -> Application:
     return SentenceTransformersModelDeployment.bind(  # type: ignore[attr-defined]

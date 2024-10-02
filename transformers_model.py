cd032e257e306d4e1c9ccdc969169624ee431db3
Korin Thompson <42357484+korinayo@users.noreply.github.com>
2024-10-02 08:29:02 -0700

    transformers model

--- transformers_model.py
+++ transformers_model.py
@@ -4,7 +4,7 @@ from ray import serve
 from ray.serve import Application
 from transformers import pipeline
 
-from utils import InferenceRequest, numpy_to_std
+from utils import InferenceRequest, numpy_to_std, dtype_mapping
 
 web_app = FastAPI()
 
@@ -12,17 +12,26 @@ web_app = FastAPI()
 @serve.deployment
 @serve.ingress(web_app)
 class TransformersModelDeployment:
-    def __init__(self, model_path: str, task: str, trust_remote_code: bool):
+    def __init__(self, model_path: str, task: str, trust_remote_code: bool, torch_dtype: str):
         self.model_path = model_path
         self.task = task
         self.trust_remote_code = trust_remote_code
-        self.pipe = pipeline(
-            task=self.task,
-            model=self.model_path,
-            trust_remote_code=self.trust_remote_code,
-            low_mem_cpu_usage=True,
-            device="cuda" if torch.cuda.is_available() else "cpu",
-        )
+        self.torch_dtype = torch_dtype
+        pipe_kwargs = {
+            "task": self.task,
+            "model": self.model_path,
+            "trust_remote_code": self.trust_remote_code,
+            "low_mem_cpu_usage": True,
+            "device": "cuda" if torch.cuda.is_available() else "cpu",
+        }
+
+        if torch_dtype:
+            pipe_kwargs["torch_dtype"] = dtype_mapping.get(torch_dtype.lower(), None)
+
+        # No need to call .eval() here, since pipeline does it for us
+        # https://github.com/huggingface/transformers/blob/main/src/transformers/pipelines/base.py#L287-L290
+        self.pipe = pipeline(**pipe_kwargs)
+
 
     @web_app.get("/model_device")
     def model_device(self) -> str:
@@ -38,8 +47,11 @@ class TransformersModelDeployment:
     def model_config(self):
         return numpy_to_std(self.pipe.model.config.__dict__)
 
+    @web_app.get("/device_map")
+    def device_map(self) -> dict:
+        return numpy_to_std(self.pipe.model.hf_device_map)
 
 def app_builder(args: dict) -> Application:
     return TransformersModelDeployment.bind(  # type: ignore[attr-defined]
-        args["model_path"], args["task"], args["trust_remote_code"]
+        args["model_path"], args["task"], args["trust_remote_code"], args["torch_dtype"]
     )

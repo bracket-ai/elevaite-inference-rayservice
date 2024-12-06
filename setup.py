b544dc2fc08e6302712724f111fa290370e41d5c
Korin Thompson <42357484+korinayo@users.noreply.github.com>
2024-12-06 09:29:11 -0800

    package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="inference_rayservice",
+    version="0.1.0",
+    description="Code for the Elevaite Inference Ray Service",
+    author="Connor Boyle, Korin Thompson",
+    packages=find_packages(),
+    install_requires=[
+        "ray[serve]>=2.37.0",
+        "transformers>=4.45.1",
+        "torch>=2.4.1",
+        "fastapi>=0.115.0",
+        "numpy>=2.1.1",
+    ],
+    python_requires=">=3.11.9",
+)
